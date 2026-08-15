79010d8a417ef2d3ea44559dd8875ca5e6ab84fd
bishirkdy <bishirbishi1@gmail.com>
2026-08-15 17:27:12 +0530

    added console for testing cors issue test 8

--- src/AquaticFishECommerce.API/Extensions/CorsExtensions.cs
+++ src/AquaticFishECommerce.API/Extensions/CorsExtensions.cs
@@ -1,28 +1,19 @@
 namespace AquaticFishECommerce.API.Extensions
 {
-    public static class CorsExtensions
+    public static  class CorsExtensions
     {
-        public static IServiceCollection AddCorsPolicy(
-            this IServiceCollection services,
-            IConfiguration configuration)
+        public static IServiceCollection AddCorsPolicy(this IServiceCollection services , IConfiguration configuration)
         {
-            var allowedOrigin =
-                Environment.GetEnvironmentVariable("Cors__AllowedOrigins__0");
-
-            File.WriteAllText(
-                @"C:\inetpub\AquaticFishECommerce\cors-debug.txt",
-                $"CORS Origin: {allowedOrigin}"
-            );
-
+            var allowedOrigins = configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
+            //Register frondent with policy
             services.AddCors(options =>
             {
                 options.AddPolicy("ReactPolicy", policy =>
                 {
-                    policy
-                        .WithOrigins(allowedOrigin ?? "")
-                        .AllowAnyHeader()
-                        .AllowAnyMethod()
-                        .AllowCredentials();
+                    policy.WithOrigins(allowedOrigins ?? Array.Empty<string>())
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
                 });
             });
 
